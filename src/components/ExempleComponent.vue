a0111bcf6336b9a594a33ad3b7f4e499d1a85ca7
Rafael <francadev@hotmail.com>
2023-11-10 11:31:23 -0300

    clear all project and config eslint

---
+++ src/components/ExempleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1></h1>
+</template>
+
+<script>
+export default {
+  name: 'ExempleComponent',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,15 +1,13 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <h1>test</h1>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  methods: {
+  }
 }
 </script>
